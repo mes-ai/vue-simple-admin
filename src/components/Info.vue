469ce822866946b8af4bf207b5ae4a8018cc3215
mes-ai <395015557@qq.com>
2020-12-28 00:59:32 +0800

    add component and vue

---
+++ src/components/Info.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="info">
+    <h1>{{ msg }}</h1>
+    <span> this is div</span>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Info',
+  props: {
+    msg: String
+  }
+}
+</script>
---
+++ src/views/MSG.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="info">
+    <h1>This is an MSG page</h1>
+     <Info msg="this is info component"/>
+  </div>
+</template>
+<script>
+// @ is an alias to /src
+import Info from '@/components/Info.vue'
+export default {
+  name: 'Msg',
+  components: {
+    Info
+  }
+}
+</script>
